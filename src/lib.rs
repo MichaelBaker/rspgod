deb16d1de44bd18fe5a7b5d9f84f87395e462812
Josh Cheek <josh.cheek@gmail.com>
2015-05-24 22:15:07 -0600

    Uses automatically generated Postgres data structures in our callbacks

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,21 @@
+extern crate libc;
+
 use std::ffi::CString;
+mod postgres;
+
+use postgres::{LogicalDecodingContext, OutputPluginOptions, OUTPUT_PLUGIN_TEXTUAL_OUTPUT};
 
 extern {
   fn OutputPluginPrepareWrite(ctx: &LogicalDecodingContext, last_write: bool);
   fn OutputPluginWrite(ctx: &LogicalDecodingContext, last_write: bool);
-  fn appendStringInfoString(str: &DontTouch, s: *const i8);
+  fn appendStringInfoString(str: *mut postgres::Struct_StringInfoData, s: *const i8);
 }
 
 pub struct DontTouch;
 
-#[repr(C)]
-pub struct LogicalDecodingContext<'a> {
-    memory_context:        &'a DontTouch,
-    reader:                &'a DontTouch,
-    slot:                  &'a DontTouch,
-    reorder:               &'a DontTouch,
-    snapshot_builder:      &'a DontTouch,
-    callbacks:             OutputPluginCallbacks<'a>,
-    options:               OutputPluginOptions,
-    output_plugin_options: &'a DontTouch,
-    prepare_write:         &'a DontTouch,
-    write:                 &'a DontTouch,
-    out:                   &'a DontTouch,
-    output_plugin_private: &'a DontTouch,
-    output_writer_private: &'a DontTouch,
-    accept_writes:         bool,
-    prepared_write:        bool,
-    write_location:        u64,
-    write_xid:             u32,
-}
-
-#[repr(C)]
-pub struct OutputPluginCallbacks<'a> {
-    startup_cb:  &'a DontTouch,
-    begin_cb:    &'a DontTouch,
-    change_cb:   &'a DontTouch,
-    commit_cb:   &'a DontTouch,
-    shutdown_cb: &'a DontTouch,
-}
-
-#[repr(C)]
-pub struct OutputPluginOptions {
-    output_type: OutputPluginOutputType,
-}
-
-#[repr(C)]
-pub enum OutputPluginOutputType {
-    OUTPUT_PLUGIN_BINARY_OUTPUT,
-    OUTPUT_PLUGIN_TEXTUAL_OUTPUT,
-}
-
-
 #[no_mangle]
 pub extern fn pg_decode_startup(ctx: &LogicalDecodingContext, opt: &mut OutputPluginOptions, is_init: bool) {
-	opt.output_type = OutputPluginOutputType::OUTPUT_PLUGIN_TEXTUAL_OUTPUT;
+	opt.output_type = OUTPUT_PLUGIN_TEXTUAL_OUTPUT;
 }
 
 #[no_mangle]
