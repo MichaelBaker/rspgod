3cc48bf4d4d1bb1f084f5ecf80053d4214ac2c01
Josh Cheek <josh.cheek@gmail.com>
2015-05-24 22:39:58 -0600

    Turns of tons of Rust linter warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
 extern crate libc;
 
 use std::ffi::CString;
@@ -20,7 +26,7 @@ pub extern fn pg_decode_startup(ctx: &LogicalDecodingContext, opt: &mut OutputPl
 
 #[no_mangle]
 pub extern fn pg_decode_change(ctx: &LogicalDecodingContext, a: &DontTouch, b: &DontTouch, c: &DontTouch) {
-    let to_print = &b"Hello, world! -- rust"[..];
+    let to_print = &b"OMG, world! -- rust"[..];
     let c_to_print = CString::new(to_print).unwrap();
 
     unsafe {
