5d38a02c22cad3c75530225f9ddf90086ba34427
Michael Baker <michaeltbaker@gmail.com>
2015-06-04 20:24:58 -0500

    Adds a replication slot test

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,8 @@
 extern crate postgres;
 
 use postgres::{Connection, SslMode};
+use std::thread;
+
 
 #[derive(Debug)]
 struct TestRecord {
@@ -12,21 +14,40 @@ struct TestRecord {
 fn sanity_test() {
     with_clean_database(|c| {
         let records = vec![
-            TestRecord {
-                id:   1,
-                name: "Michael Baker".to_string(),
-            },
-            TestRecord {
-                id:   2,
-                name: "Josh Cheek".to_string(),
-            },
+            TestRecord { id: 1, name: "Michael Baker".to_string() },
+            TestRecord { id: 2, name: "Josh Cheek".to_string(),   },
         ];
 
         for record in records.iter() {
             create_record(c, &record);
         }
+
         fetch_records(c);
     });
+}
+
+#[test]
+fn basic_logical_decoding() {
+    with_clean_database(|c| {
+        let record = TestRecord {id: 1, name: "Michael Baker".to_string(), };
+        drop_slot(c);
+        create_slot(c);
+        create_record(c, &record);
+    });
+}
+
+//
+// [TODO] I want to move a lot of these into a utility module when I can figure out how to do that
+//
+
+fn create_slot(c: &Connection) {
+    let stmt = c.prepare("select * from pg_create_logical_replication_slot('slot', 'thingy')").unwrap();
+    stmt.execute(&[]).unwrap();
+}
+
+fn drop_slot(c: &Connection) {
+    let stmt = c.prepare("select pg_drop_replication_slot('slot')").unwrap();
+    stmt.execute(&[]).unwrap();
 }
 
 fn with_clean_database<F>(f: F) where F:Fn(&Connection) -> () {
@@ -42,12 +63,12 @@ fn reset_database(c: &Connection) {
 }
 
 fn create_database(c: &Connection) {
-    let stmt = c.prepare("create table test (id int primary key, name text)").unwrap();
+    let stmt = c.prepare("create table test_table (id int primary key, name text)").unwrap();
     stmt.execute(&[]).unwrap();
 }
 
 fn fetch_records(c: &Connection) -> Vec<TestRecord> {
-    let stmt = c.prepare("select id, name from test").unwrap();
+    let stmt = c.prepare("select id, name from test_table").unwrap();
     let results = stmt.query(&[]).unwrap();
     results.iter().map(|r| {
         TestRecord {
@@ -58,7 +79,7 @@ fn fetch_records(c: &Connection) -> Vec<TestRecord> {
 }
 
 fn create_record(c: &Connection, r: &TestRecord) {
-    let stmt = c.prepare("insert into test (id, name) values ($1, $2)").unwrap();
+    let stmt = c.prepare("insert into test_table (id, name) values ($1, $2)").unwrap();
     stmt.execute(&[
         &r.id,
         &r.name,
@@ -66,7 +87,7 @@ fn create_record(c: &Connection, r: &TestRecord) {
 }
 
 fn drop_database(c: &Connection) {
-    let stmt = c.prepare("drop table if exists test").unwrap();
+    let stmt = c.prepare("drop table if exists test_table").unwrap();
     stmt.execute(&[]).unwrap();
 }
 
