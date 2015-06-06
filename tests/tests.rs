16b3261246016de796a8cc6fa1b03bea6e575da8
Michael Baker <michaeltbaker@gmail.com>
2015-06-06 14:35:02 -0500

    Moves all of the test helpers into their own module

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,15 +1,20 @@
 extern crate postgres;
 extern crate rustc_serialize;
 
-use postgres::{Connection, SslMode};
-use postgres::types::{ToSql};
-use rustc_serialize::json::Json;
+mod utils;
 
-#[derive(Debug)]
-struct TestRecord {
-    id:   i32,
-    name: String,
-}
+use rustc_serialize::json::Json;
+use utils::{
+    fetch_updates,
+    fetch_records,
+    with_slot,
+    with_clean_database,
+    execute,
+    create_record,
+    update_record,
+    delete_record,
+    TestRecord,
+};
 
 #[test]
 fn sanity_test() {
@@ -80,103 +85,4 @@ fn delete_without_primary_key() {
         execute(c, "delete from no_primary_table where id = 1", &[]);
         assert_eq!(fetch_updates(c).len(), 1);
     });
-}
-
-//
-// [TODO] I want to move a lot of these into a utility module when I can figure out how to do that
-//
-
-fn execute(c: &Connection, command: &str, args: &[&ToSql]) {
-    let stmt = c.prepare(command).unwrap();
-    stmt.execute(args).unwrap();
-}
-
-fn execute_silent(c: &Connection, command: &str, args: &[&ToSql]) {
-    let stmt = c.prepare(command).unwrap();
-    match stmt.execute(args) { _ => {} };
-}
-
-fn fetch_updates(c: &Connection) -> Vec<String> {
-    let stmt = c.prepare("SELECT * FROM pg_logical_slot_peek_changes('slot', NULL, NULL)").unwrap();
-    stmt.query(&[]).unwrap().iter().map(|r| { r.get(2) }).collect()
-}
-
-fn create_slot(c: &Connection) {
-    execute(c, "select * from pg_create_logical_replication_slot('slot', 'thingy')", &[]);
-}
-
-fn drop_slot(c: &Connection) {
-    execute_silent(c, "select pg_drop_replication_slot('slot')", &[]);
-}
-
-fn with_slot<F>(table_name: &str, columns: &str, f: F) where F:Fn(&Connection) -> () {
-    with_clean_database(table_name, columns, |c| {
-        drop_slot(c);
-        create_slot(c);
-        f(c);
-        drop_slot(c);
-    });
-}
-
-fn with_clean_database<F>(table_name: &str, columns: &str, f: F) where F:Fn(&Connection) -> () {
-    let c = connection();
-    reset_database(&c, table_name, columns);
-    f(&c);
-    drop_database(&c, table_name);
-}
-
-fn reset_database(c: &Connection, table_name: &str, columns: &str) {
-    drop_database(&c, table_name);
-    create_database(&c, table_name, columns);
-}
-
-fn create_database(c: &Connection, table_name: &str, columns: &str) {
-    execute(c, &format!("create table {} ({})", table_name, columns), &[]);
-}
-
-fn fetch_records(c: &Connection) -> Vec<TestRecord> {
-    let stmt = c.prepare("select id, name from test_table").unwrap();
-    let results = stmt.query(&[]).unwrap();
-    results.iter().map(|r| {
-        TestRecord {
-            id:   r.get(0),
-            name: r.get(1),
-        }
-    }).collect()
-}
-
-fn create_record(c: &Connection, r: &TestRecord) {
-    execute(c, "insert into test_table (id, name) values ($1, $2)", &[
-        &r.id,
-        &r.name,
-    ]);
-}
-
-fn delete_record(c: &Connection, id: i32) {
-    execute(c, "delete from test_table where id = $1", &[&id]);
-}
-
-fn update_record(c: &Connection, new_record: TestRecord) {
-    execute(c, "update test_table set name = $2 where id = $1", &[
-        &new_record.id,
-        &new_record.name
-    ]);
-}
-
-fn drop_database(c: &Connection, table_name: &str) {
-    execute(c, &format!("drop table if exists {}", table_name), &[]);
-}
-
-fn connection_string() -> String {
-    match std::env::var("POSTGRES_URL") {
-        Ok(val) => val,
-        Err(e)  => panic!(format!("You must set the POSTGRES_URL environment variable to point to a running Postgres test database when running automated tests: {}", e)),
-    }
-}
-
-fn connection() -> Connection {
-    match Connection::connect(&connection_string()[..], &SslMode::None) {
-        Ok(c)  => c,
-        Err(e) => panic!(format!("Could not connect to the test database: {}", e)),
-    }
 }
---
+++ tests/utils.rs
@@ -0,0 +1,104 @@
+use std::env;
+use postgres::{Connection, SslMode};
+use postgres::types::{ToSql};
+
+#[derive(Debug)]
+pub struct TestRecord {
+    pub id:   i32,
+    pub name: String,
+}
+
+pub fn execute(c: &Connection, command: &str, args: &[&ToSql]) {
+    let stmt = c.prepare(command).unwrap();
+    stmt.execute(args).unwrap();
+}
+
+pub fn execute_silent(c: &Connection, command: &str, args: &[&ToSql]) {
+    let stmt = c.prepare(command).unwrap();
+    match stmt.execute(args) { _ => {} };
+}
+
+pub fn fetch_updates(c: &Connection) -> Vec<String> {
+    let stmt = c.prepare("SELECT * FROM pg_logical_slot_peek_changes('slot', NULL, NULL)").unwrap();
+    stmt.query(&[]).unwrap().iter().map(|r| { r.get(2) }).collect()
+}
+
+pub fn create_slot(c: &Connection) {
+    execute(c, "select * from pg_create_logical_replication_slot('slot', 'thingy')", &[]);
+}
+
+pub fn drop_slot(c: &Connection) {
+    execute_silent(c, "select pg_drop_replication_slot('slot')", &[]);
+}
+
+pub fn with_slot<F>(table_name: &str, columns: &str, f: F) where F:Fn(&Connection) -> () {
+    with_clean_database(table_name, columns, |c| {
+        drop_slot(c);
+        create_slot(c);
+        f(c);
+        drop_slot(c);
+    });
+}
+
+pub fn with_clean_database<F>(table_name: &str, columns: &str, f: F) where F:Fn(&Connection) -> () {
+    let c = connection();
+    reset_database(&c, table_name, columns);
+    f(&c);
+    drop_database(&c, table_name);
+}
+
+pub fn reset_database(c: &Connection, table_name: &str, columns: &str) {
+    drop_database(&c, table_name);
+    create_database(&c, table_name, columns);
+}
+
+pub fn create_database(c: &Connection, table_name: &str, columns: &str) {
+    execute(c, &format!("create table {} ({})", table_name, columns), &[]);
+}
+
+pub fn fetch_records(c: &Connection) -> Vec<TestRecord> {
+    let stmt = c.prepare("select id, name from test_table").unwrap();
+    let results = stmt.query(&[]).unwrap();
+    results.iter().map(|r| {
+        TestRecord {
+            id:   r.get(0),
+            name: r.get(1),
+        }
+    }).collect()
+}
+
+pub fn create_record(c: &Connection, r: &TestRecord) {
+    execute(c, "insert into test_table (id, name) values ($1, $2)", &[
+        &r.id,
+        &r.name,
+    ]);
+}
+
+pub fn delete_record(c: &Connection, id: i32) {
+    execute(c, "delete from test_table where id = $1", &[&id]);
+}
+
+pub fn update_record(c: &Connection, new_record: TestRecord) {
+    execute(c, "update test_table set name = $2 where id = $1", &[
+        &new_record.id,
+        &new_record.name
+    ]);
+}
+
+pub fn drop_database(c: &Connection, table_name: &str) {
+    execute(c, &format!("drop table if exists {}", table_name), &[]);
+}
+
+pub fn connection_string() -> String {
+    match env::var("POSTGRES_URL") {
+        Ok(val) => val,
+        Err(e)  => panic!(format!("You must set the POSTGRES_URL environment variable to point to a running Postgres test database when running automated tests: {}", e)),
+    }
+}
+
+pub fn connection() -> Connection {
+    match Connection::connect(&connection_string()[..], &SslMode::None) {
+        Ok(c)  => c,
+        Err(e) => panic!(format!("Could not connect to the test database: {}", e)),
+    }
+}
