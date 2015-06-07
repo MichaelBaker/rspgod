8d381f480d6455fa456b3e594db0e7921fb849a2
Michael Baker <michaeltbaker@gmail.com>
2015-06-06 19:11:25 -0500

    Adds the remaining core types to the test

--- tests/edge_cases.rs
+++ tests/edge_cases.rs
@@ -17,78 +17,102 @@ fn delete_without_primary_key() {
     });
 }
 
-// TODO: Add these all to the test
-// -------------------------------
-// date
-// double precision
-// inet
-// integer
-// interval [ fields ] [ (p) ]
-// json
-// jsonb
-// line
-// lseg
-// macaddr
-// money
-// numeric [ (p, s) ]
-// path
-// pg_lsn
-// point
-// polygon
-// real
-// text
-// time [ (p) ] [ without time zone ]
-// time [ (p) ] with time zone
-// timestamp [ (p) ] [ without time zone ]
-// timestamp [ (p) ] with time zone
-// tsquery
-// tsvector
-// txid_snapshot
-// uuid
-// xml
 #[test]
 fn works_with_all_datatypes() {
-    let columns = "                        \
-        id              int primary key,   \
-        abool           boolean,           \
-        anint           int,               \
-        abigint         bigint,            \
-        asmallint       smallint,          \
-        aserialnint     serial,            \
-        aserialbigint   bigserial,         \
-        aserialsmallint smallserial,       \
-        somebits        BIT(3),            \
-        somemorebits    bit varying,       \
-        abox            box,               \
-        somebytes       bytea,             \
-        somechars       character(3),      \
-        morechars       character varying, \
-        addr            cidr,              \
-        circ            circle
+    let columns = "
+        id              int primary key,
+        abool           boolean,
+        anint           int,
+        abigint         bigint,
+        asmallint       smallint,
+        aserialnint     serial,
+        aserialbigint   bigserial,
+        aserialsmallint smallserial,
+        somebits        BIT(3),
+        somemorebits    bit varying,
+        abox            box,
+        somebytes       bytea,
+        somechars       character(3),
+        morechars       character varying,
+        addr            cidr,
+        circ            circle,
+        adate           date,
+        afloat          double precision,
+        aninet          inet,
+        aninteger       integer,
+        ayear           interval year,
+        somejson        json,
+        somejsonb       jsonb,
+        aline           line,
+        alineseg        lseg,
+        amac            macaddr,
+        somedollers     money,
+        anumber         numeric(2, 2),
+        apath           path,
+        alsn            pg_lsn,
+        thepoint        point,
+        apoly           polygon,
+        thereal         real,
+        sometext        text,
+        thetimezone     time without time zone,
+        thetimenozone   time with time zone,
+        timestampzone   timestamp without time zone,
+        timestampnozone timestamp with time zone,
+        aquery          tsquery,
+        avec            tsvector,
+        txid            txid_snapshot,
+        auuid           uuid,
+        somexml         xml
     ";
 
-    let values = "             \
-        1,                     \
-        true,                  \
-        1,                     \
-        1,                     \
-        1,                     \
-        1,                     \
-        1,                     \
-        1,                     \
-        B'101',                \
-        B'101',                \
-        '((0,0),(1,1))',       \
-        E'\x0123456789ABCDEF', \
-        'abc',                 \
-        'abcdef',              \
-        '192.168.100.128/25',  \
-        '((0,0), 1)'           \
+    let values = "
+        1,
+        true,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        B'101',
+        B'101',
+        '((0,0),(1,1))',
+        E'\x0123456789ABCDEF',
+        'abc',
+        'abcdef',
+        '192.168.100.128/25',
+        '((0,0), 1)',
+        '1999-01-08',
+        1.2,
+        '192.168.100.128/25',
+        1,
+        '1',
+        '{\"a\":123}',
+        '{\"a\":123}',
+        '((0,0),(1,1))',
+        '((0,0),(1,1))',
+        '08:00:2b:01:02:03',
+        '12.23',
+        0.1,
+        '((0,0),(1,1))',
+        '16/B374D848',
+        '(0,0)',
+        '((0,0),(1,1))',
+        1.39284,
+        'what we do in life echos in eternity',
+        '04:05:06',
+        '04:05:06-08:00',
+        '1999-01-08 04:05:06',
+        '1999-01-08 04:05:06-08:00',
+        'fat & rat',
+        'fat rat',
+        '10:20:10,14,15',
+        'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
+        '<foo>bar</foo>'
     ";
 
     with_slot("datatypes", columns, |c| {
         execute(c, &format!("insert into datatypes values ({})", values)[..], &[]);
-        println!("{}", as_pretty_json(&fetch_updates(c)));
         assert_eq!(fetch_updates(c).len(), 1);
     });
 }
